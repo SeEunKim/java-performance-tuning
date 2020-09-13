0d95de44e02812cd9e73e5b20d83519c4f4dce8d
김세은 <kimse9450@gmail.com>
2020-09-13 21:47:29 +0900

    data access object pattern

---
+++ src/main/java/example/ch01/dataaccessobjectpattern/StudentDao.java
@@ -0,0 +1,13 @@
+package example.ch01.dataaccessobjectpattern;
+
+import java.util.List;
+
+public interface StudentDao {
+    List<Student> getAllStudents();
+
+    Student getStudent(int rollNo);
+
+    void updateStudent(Student student);
+
+    void deleteStudent(Student student);
+}
